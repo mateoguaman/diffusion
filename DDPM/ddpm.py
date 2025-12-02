726eb891895244c2e00e17301e41f8524e6b6bb7
Mateo <mateo.guaman1998@gmail.com>
2025-12-02 12:53:47 -0500

    Finished UNet implementation, need to write implementation notes

--- DDPM/ddpm.py
+++ DDPM/ddpm.py
@@ -1,4 +1,5 @@
-from abc import abstractclassmethod, abstractmethod
+from abc import abstractmethod
+import time
 import torch
 import torch.nn as nn
 import math
@@ -46,7 +47,73 @@ class TimestepEmbedSequential(nn.Sequential, TimestepBlock):
             else:
                 x = module(x)
         return x
-            
+
+## NOTE: Implemented this fourth
+class Downsample(nn.Module):
+    def __init__(self, num_channels, use_conv):
+        if use_conv:
+            self.downsample_layer = nn.Conv2d(num_channels, num_channels, kernel_size=3, stride=2, padding=1)
+        else:
+            self.downsample_layer = nn.AvgPool2d(kernel_size=2) 
+
+    def forward(self, x):
+        return self.downsample_layer(x)
+
+class Upsample(nn.Module):
+    def __init__(self, num_channels, use_conv):
+        self.use_conv = use_conv
+        if use_conv:
+            self.conv = nn.Conv2d(num_channels, num_channels, kernel_size=3, padding=1)
+    
+    def forward(self, x):
+        x = nn.functional.interpolate(x, scale_factor=2, mode="nearest")
+        if self.use_conv:
+            x = self.conv(x)
+        return x
+
+
+## NOTE: Implemented this fifth
+class QKVAttention(nn.Module):
+    def forward(self, qkv):
+        '''
+        :param qkv: a (B, 3C, HxW) tensor of Qs, Ks, and Vs
+        :return: a (B, C, HxW) tensor after self-attention 
+        '''
+        channel_dim = qkv.shape[1]//3
+        Q, K, V = torch.split(qkv, [channel_dim]*3, dim=1)  # Each should have dimensions (B, C, HxW)
+        Q, K, V = Q.permute(0, 2, 1), K.permute(0, 2, 1), V.permute(0, 2, 1)
+        
+        attention = nn.functional.softmax((1/math.sqrt(channel_dim))*(Q@torch.transpose(K, 1, 2)), dim=-1)@V
+        output = attention.permute(0, 2, 1)
+
+        return output
+
+class AttentionBlock(nn.Module):
+    def __init__(self, num_channels, heads=1, groups=32):
+        self.num_heads = heads
+        self.qkv = nn.Conv1d(in_channels=num_channels, out_channels=3*num_channels, kernel_size=1)
+        self.proj_out = nn.Conv1d(in_channels=num_channels, out_channels=num_channels, kernel_size=1)
+        self.attention = QKVAttention()
+        self.norm = nn.GroupNorm(num_groups=groups, num_channels=num_channels)
+
+    def forward(self, x):
+        # Flatten H, W dimensions
+        b, c, *spatial_dims = x.shape  # x is (B, C, H, W)
+        x = x.reshape(b, c, -1)  # (B, C, L), L=H*W
+        # Normalize and Get QKV weights from input x:
+        qkv = self.qkv(self.norm(x))  # (B, 3C, L)
+        # Batch folding, i.e., split channels into different heads
+        qkv = qkv.reshape(b*self.num_heads, -1, qkv.shape[2])  # (B*num_heads, 3C//num_heads, L)
+        # Run normalization and attention (in PreNorm order from https://arxiv.org/pdf/1910.05895)
+        attention = self.attention(qkv)  # (B*num_heads, C//num_heads, L)
+        # Concatenate multiple heads
+        attention = attention.reshape(b, -1, attention.shape[-1])  # (B, C, L)
+        # Project the attention output
+        output = self.proj_out(attention)  # (B, C, L)
+        # Run residual connection and unflatten
+        output = (output + x).reshape(b, c, *spatial_dims)  # (B, C, H, W)
+
+        return output
 
 
 ## NOTE: Could make this a more general unet ddpm by defining the ResnetBlock wrt a conv dim, and having a helper function which defines the conv1d, conv2d, or conv3d based on the dim
@@ -105,6 +172,182 @@ class ResnetBlock(TimestepBlock):
 
         return out
         
+## NOTE: Implemented this sixth
+class UNet(nn.Module):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        model_channels,
+        channel_multipliers,
+        num_res_blocks,
+        groups,
+        dropout,
+        use_scale_shift,
+        attention_resolutions,
+        use_conv,
+        num_heads
+        ):
+        '''
+        :param in_channels: An int for the number of channels in the input data (3 for RGB images)
+        :param out_channels: An int for the number of output channels (3 if learning only mean, 6 if learning std)
+        :param model_channels: Initial channel dimension of the model
+        :param channel_multipliers: A list of multipliers, one per level, that the initial num_channels will be multiplied to
+        :param groups: Number of groups for group normalization. The DDPM paper sets this to 32
+        :param num_res_blocks: int of th enumber of resnet blocks to be applied per level
+        :param dropout: float [0, 1] of dropout probability
+        :param use_scale_shift: Bool of whether to use FiLM (scale + shift) for time embedding or just add
+        :param attention_resolutions: a list of resolutions (in spatial dims) at which an attention block should be applied 
+        :param use_conv: boolean of whther to use a convolutional layer for downsampling or upsampling, or use non-learned operations only (AvgPool for downscale and interpolate for upsample)
+        :param num_heads: Number of heads for Multi-Head Attention
+        '''
+        super().__init__()
+
+
+        self.model_channels = model_channels
+
+        # Shared time projector
+        time_emb_dim = 4 * model_channels
+        self.shared_time_proj = nn.Sequential(
+            nn.Linear(in_features=model_channels, out_features=time_emb_dim),
+            nn.SiLU(),
+            nn.Linear(in_features=time_emb_dim, out_features=time_emb_dim)
+        )
+
+        # Downsampling block
+        self.input_blocks = nn.ModuleList(
+            [
+                TimestepEmbedSequential(
+                    # Initial input projector
+                    nn.Conv2d(in_channels=in_channels, out_channels=model_channels, kernel_size=3, padding=1)
+                )
+            ]
+        )
+        input_blocks_channels = [model_channels]
+
+        level_channels = model_channels
+        ds = 1  # Downsampling scale
+
+        for level, mult in enumerate(channel_multipliers):
+            for j in range(num_res_blocks):
+                layers = []
+                # Create Resnet Block
+                layers.append(
+                    ResnetBlock(
+                        in_channels=level_channels, 
+                        out_channels=mult*model_channels, 
+                        groups=groups,
+                        time_emb_dim=time_emb_dim,
+                        dropout=dropout,
+                        use_scale_shift=use_scale_shift)
+                    )
+                level_channels = mult*model_channels
+
+                # Create Attention block if at the right resolution
+                if ds in attention_resolutions:
+                    layers.append(
+                        AttentionBlock(level_channels, num_heads, groups)
+                    )
+
+                self.input_blocks.append(TimestepEmbedSequential(*layers))
+                input_blocks_channels.append(level_channels)
+            if level != len(channel_multipliers) - 1:
+                self.input_blocks.append(
+                    TimestepEmbedSequential(
+                        Downsample(level_channels, use_conv)
+                    )
+                )
+                input_blocks_channels.append(level_channels)
+                ds *= 2
+
+        # Middle Block:
+        self.middle_block = TimestepEmbedSequential(
+            ResnetBlock(
+                in_channels=level_channels,
+                out_channels=level_channels,
+                groups=groups,
+                time_emb_dim=time_emb_dim,
+                dropout=dropout,
+                use_scale_shift=use_scale_shift
+            ),
+            AttentionBlock(
+                num_channels=level_channels,
+                heads=num_heads,
+                groups=groups
+            ),
+            ResnetBlock(
+                in_channels=level_channels,
+                out_channels=level_channels,
+                groups=groups,
+                time_emb_dim=time_emb_dim,
+                dropout=dropout,
+                use_scale_shift=use_scale_shift
+            )
+        )
+
+        # Need an upsampling block
+        self.output_blocks = nn.ModuleList()
+        for level, mult in list(enumerate(channel_multipliers))[::-1]:
+            for j in range(num_res_blocks + 1):
+                layers = []
+                # Create Resnet Block
+                layers.append(
+                    ResnetBlock(
+                        in_channels=level_channels+input_blocks_channels.pop(), 
+                        out_channels=mult*model_channels, 
+                        groups=groups,
+                        time_emb_dim=time_emb_dim,
+                        dropout=dropout,
+                        use_scale_shift=use_scale_shift)
+                    )
+                level_channels = mult*model_channels
+
+                # Create Attention block if at the right resolution
+                if ds in attention_resolutions:
+                    layers.append(
+                        AttentionBlock(level_channels, num_heads, groups)
+                    )
+                if level != 0 and j == num_res_blocks:
+                    layers.append(
+                        Upsample(num_channels=level_channels, use_conv=use_conv)
+                    )
+                    ds //= 2
+                self.output_blocks.append(TimestepEmbedSequential(*layers))
+
+        self.out = nn.Sequential(
+            nn.GroupNorm(groups, num_channels=level_channels),
+            nn.SiLU(),
+            nn.Conv2d(in_channels=level_channels, out_channels=out_channels, kernel_size=3, padding=1)
+        )
+
+    def forward(self, x, t):
+        '''
+        :param x: a (B, 3, H, W) tensor
+        :param t: a (B, 1) tensor of timesteps
+        '''
+        # Embed the timesteps using sinusoidal positional encoding + shared projector, to get (B, 4*D), where D comes from sinusoidal encoding, 4 comes from the shared projection
+        encoded_t = time_embedding(t, self.model_channels)
+        time_emb = self.shared_time_proj(encoded_t)
+
+        # pass through ModuleList
+        hs = []
+        for module in self.input_blocks:
+            x = module(x, time_emb)
+            hs.append(x)
+
+        x = self.middle_block(x, time_emb)
+
+        for i, module in enumerate(self.output_blocks):
+            skip_connection = hs.pop()
+            x = torch.cat([x, skip_connection], dim=1)
+            x = module(x, time_emb)
+
+        out = self.out(x)
+
+        return out
+
+        
+
 
 if __name__ == "__main__":
     timesteps = torch.Tensor([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
--- DDPM/implementation_notes.md
+++ DDPM/implementation_notes.md
@@ -11,7 +11,6 @@ blocks [6]. Diffusion time t is specified by adding the Transformer sinusoidal p
 into each residual block. 
 ```
 
-
 There are a bunch of details here that matter. First of all, U-Net consists of residual blocks of 2 3x3 convolution (with padding=1 to simplify residual connections in the U-net, even though in the original U-net there is no padding). Even though you could implement something like 2*(conv-norm-FiLM-act), in the original implementation they do something a bit different. First, they assume that you first have a convolution layer that goes from original image channels to network channels, and then the architecture looks like this: (norm-act-conv)-(norm-FiLM-act-dropout-conv). So they only really apply FiLM once per resnet block, and they add dropout only in the second conv block. Additionally, the output of this block has a residual connection, such that if the output of the above combined architecture is h, then the output of the layer is `h + skip_connection(x)`, where x is the original input, `skip_connection` is either an identity layer if input channels is the same as output channels, or a convolutional layer with 1x1 kernel if input channels is different than output channels.
 
 
@@ -29,3 +28,28 @@ For me, it is a bit tough to parse what the positional embeddings are supposed t
 ## On TimestepBlock and TimestepEmbedSequential
 
 In the original implementation, the authors define these two classes. The point of `TimestepBlock` is to simply define a module which will take as input not only `x` but also `time_emb`. This will be used for checking it any given module in a `Sequential` block is a `TimestepBlock` or not, and the job of `TimestepEmbedSequential` is to effectively do an if statement for each layer in the `Sequential` block, and if the layer is a `TimestepBlock`, then run it with the `x` and `time_emb` inputs, otherwise just run with the `x` inputs. 
+
+## Downsampling
+
+The original U-net code uses 2x2 maxpool operations with a stride of 2 to downsample a residual block, and also doubles the number of channels. However, in the improved DDPM implementation, there are two options. The default option (if `use_conv=True`) is to use a learned convolutional layer to downsample by using a stride of 2 and 3x3 kernel with padding=1. The other option is to simply use an `AvgPool2d` operation with kernel size 2. Interestingly, this downsample operation keeps the number of channels constant, and its only job is to downsample the height and width dimensions without also changing the channel dimension. To change channel dimension, the original codebase uses another ResNet block.
+
+## Upsampling 
+
+To upsample, the original codebase uses `nn.functional.interpolation` with `scale_factor=2` and `mode=nearest` to upsample the height and width, rather than using something like a `ConvTranspose2d`. Optionally, the upsampled features also go through a convolutional layer with the same input and output channels, 3x3 kernel, and padding of 1.
+
+## Attention Block
+
+DDPM introduces an attention block in three places: in the downsampling block at the 16x16 resolution ResNet block, in the middle bottleneck, and in the upsampling block at the 16x16 resolution. To implement the attention blocks, we can split up the QKV Attention logic (from the Attention is all you need paper), and the logic surrounding the QKV attention block, e.g. flattening H,W dimensions, creating the QKV tensors, adding normalization and residual connection, etc. 
+
+## QKV Attention
+One important design decision is how we convert the output of a convolutional layer (B, C, H, W) into the shapes needed for the attention operation, which expects (B, L, D), where L is the sequence length and D is the dimensionality of each datapoint in the sequence. In this case, we want the attention operator to have the job of determining what other pixels in the "image" each pixel should pay attention to, so we can think of the HxW dimensions combined as the "sequence length" and the channel dimension C as the feature dimension D.
+
+So in order to convert (B, C, H, W), we first flatten the H, W dimensions into L, to get a tensor (B, C, L). The goal is to eventually obtain tensors Q, K, and V, each of which has dimensionality (B, L, C) from the input with dimensionality (B, C, L). To do this, we have two options. First, we could reshape the (B, C, L) input into (B, L, C) and create a linear layer with input dim C and output dim 3C, since linear layers operate on the last dimension and. Another option is to use a 1-dim convolution which operates on the second dimension, C, and use `in_channels=C` and `out_channels=3C`, and then reshape this (B, 3C, L) tensor into (B, L, 3C) either before or after splitting these 3C into the respective Q, K, V tensors. The original ddpm implementation uses a 1-dim convolution for this operation, so that's what I will use too. An important note is that the Q, K, V tensors expect this order (B, L, C), which matters when deciding which dimension the softmax should operate over. If we think of L as the sequence of pixels, The `QK^T` matrix is a (B, L, L) tensor where the L, L dims form a square matrix, where each row i will correspond to the probability that a given pixel should pay attention to each of the other pixels, so the softmax operation needs to happen for each row, which corresponds to the `-1` dimension, i.e. the last L in (B, L, L). Otherwise, if you operate in the (B, C, L) order directly, the attention operator will give you the probability that each dimension of the channel should attend at other channel dimensions for all elements of the sequence, which doesn't make sense. 
+
+## Attention Block
+
+Once we have the QKV attention implemented, we need the tooling around it. This involves: normalizing the input [PreNorm](https://tnq177.github.io/assets/docs/transformers_without_tears.pdf), getting the QKV tensor from the input, adding multi-head attention logic, running the QKV Attention operation, concatenating the multiple heads, projecting the output of attention with a 1D convolutional layer, and adding the residual connection. There are two things that I found interesting:
+  1. The first thing is that normalization needs to happen before we project the input into the QKV tensor. This is because otherwise, the normalization would make Q, K, and V correlated.
+  2. Second, to implement Multi-Head Attention, we can simply divide the channel dimension by the number of heads, and do batch folding, in which we concatenate the heads along the batch dimension, run the same attention mechanism as if you had done single-head attention, and then after the attention operation, reshape into the right dimensions.
+
+## UNet
